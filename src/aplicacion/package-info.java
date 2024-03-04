197dae66e0487df2825d17ac748dbb1cc10c8d92
Jorge González <jorgeglezfdez05@gmail.com>
2024-03-04 11:17:08 +0100

    Añado el package info con su respectivo comentario.

---
+++ src/aplicacion/package-info.java
@@ -0,0 +1,4 @@
+/**
+* La clase Aplicación contiene el código para interactuar con la clase matemáticas, es la interfaz de usuario que permite manejar la funcionalidad del programa.*/
+
+package aplicacion;
